a2ad28c6241e036a4c05597c514bce7161be61a2
Matheus Reis <souzareis42@gmail.com>
2026-04-09 16:53:09 -0300

    feat(p1): da vida ao personagem e permite coleta de itens

---
+++ src/Jogo.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+
+public class Jogo {
+
+    public static void main(String[] args)throws Exception {
+        var gerador = new Random();
+        var cacador = new Personagem("Cleber");
+        String[] itens = {"Pena", "Couro", "Osso", "Presa", "Garra"};
+
+        while(true){
+            int OQueFazer = gerador.nextInt(1, 4);
+            switch (OQueFazer){
+                case 1:{
+                    int QualItem = gerador.nextInt(1, 6);
+                    cacador.mochila.add(itens[QualItem-1]);
+                    cacador.cacar();
+                    break;
+                }
+                case 2:
+                    cacador.comer();
+                    break;
+                case 3:
+                    cacador.dormir();
+                    break;
+            }
+            System.out.println(cacador);
+            System.out.println("Conteudo da mochila do " + cacador.nome + ": " + cacador.mochila);
+            System.out.println("***************************");
+            try {
+                Thread.sleep(5000);
+            }catch (InterruptedException _) {
+            } 
+        }
+    }
+}
