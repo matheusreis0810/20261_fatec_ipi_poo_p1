19d1f6314ddb7927844806ff9acd23225e14b3ba
Matheus Reis <souzareis42@gmail.com>
2026-04-15 23:06:30 -0300

    feat(p1): implementa duelo musical entre personagens

--- src/Jogo.java
+++ src/Jogo.java
@@ -2,72 +2,124 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
 public class Jogo {
-
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args) throws Exception {
         var gerador = new Random();
         var cacador = new Personagem("Cleber");
         var tico = new Personagem("Tico Trovador", 3, 8, 8);
         tico.repertorio.add(new Musica("God's Plan"));
+        boolean campeao = false;
 
-        String[] itens = {"Pena", "Couro", "Osso", "Presa", "Garra"};
-        
-        List <Musica> disponivel = new ArrayList<>();
-        String[] titulos = {"Center Mass", "Purple Rain", "Kidult", "End of Beginning", "In a Lake",
-                            "Timeless", "Don't Fear The Reaper", "Karma Police", "The Boss", "Layla"};
-                            
+        String[] itens = {
+                "Pena",
+                "Couro",
+                "Osso",
+                "Presa",
+                "Garra"
+        };
+
+        List<Musica> disponivel = new ArrayList<>();
+        String[] titulos = {
+                "Center Mass",
+                "Purple Rain",
+                "Kidult",
+                "End of Beginning",
+                "In a Lake",
+                "Timeless",
+                "Don't Fear The Reaper",
+                "Karma Police",
+                "The Boss",
+                "Layla"
+        };
+
         for (int i = 0; i < 10; i++) {
             disponivel.add(new Musica(titulos[i]));
         }
-
-        while(!cacador.estaMorto() || !tico.estaMorto()){
+        System.out.println("**********");
+        while (!cacador.estaMorto() || !tico.estaMorto()) {
             int OQueFazer = gerador.nextInt(1, 4);
             int acaoTico = gerador.nextInt(1, 11);
-            
-               switch (acaoTico){
-                case 1,2:{
-                    int QualItem = gerador.nextInt(1, 6);
-                    tico.mochila.add(itens[QualItem-1]);
-                    tico.cacar();
-                    break;
-                }
-                case 3,4:
-                    tico.comer();
-                    break;
-                case 5,6,7,8,9,10:
-                    tico.dormir();
-                    break;
-            }
-            System.out.println("******************************");
-            switch (OQueFazer){
-                case 1:{
-                    int QualItem = gerador.nextInt(1, 6);
-                    cacador.mochila.add(itens[QualItem-1]);
-                    cacador.cacar();
-                    break;
-                }
-                case 2:
-                    cacador.comer();
-                    break;
-                case 3:
-                    cacador.dormir();
-                    break;
-            }
-            System.out.println("******************************");
-            System.out.println(cacador);
-            cacador.aprenderMusica(disponivel);
-            System.out.println(cacador);
-            cacador.morte(cacador.nome, cacador.mochila, cacador.repertorio);
-            System.out.println(tico);
-            tico.aprenderMusica(disponivel);
-            System.out.println(tico);
-            tico.morte(tico.nome, tico.mochila, tico.repertorio);
+            int quemDuela = gerador.nextInt(1, 3);
 
-            try {
-                Thread.sleep(5000);
-            }catch (InterruptedException _) {
-            } 
+            if (!tico.estaMorto()) {
+                switch (acaoTico) {
+                    case 1, 2: {
+                        int QualItem = gerador.nextInt(1, 6);
+                        tico.mochila.add(itens[QualItem - 1]);
+                        tico.cacar();
+                        break;
+                    }
+                    case 3, 4:
+                        tico.comer();
+                        break;
+                    case 5, 6, 7, 8, 9, 10:
+                        tico.dormir();
+                        break;
+                }
+                System.out.println(tico);
+                tico.aprenderMusica(disponivel);
+                System.out.println("Repertório: " + tico.repertorio);
+                tico.morte();
+                System.out.println("**********");
+            }
+            if (!cacador.estaMorto()) {
+                switch (OQueFazer) {
+                    case 1: {
+                        int QualItem = gerador.nextInt(1, 6);
+                        cacador.mochila.add(itens[QualItem - 1]);
+                        cacador.cacar();
+                        break;
+                    }
+                    case 2:
+                        cacador.comer();
+                        break;
+                    case 3:
+                        cacador.dormir();
+                        break;
+                }
+                System.out.println(cacador);
+                cacador.aprenderMusica(disponivel);
+                System.out.println("Repertório: " + cacador.repertorio);
+                cacador.morte();
+                System.out.println("**********");
+            }
+            if (!cacador.estaMorto() && !tico.estaMorto()) {
+                switch (quemDuela) {
+                    case 1: {
+                        tico.dueloMusical(cacador);
+                        break;
+                    }
+                    case 2: {
+                        cacador.dueloMusical(tico);
+                        break;
+                    }
+                }
+                System.out.println("**********");
+                tico.morte();
+                cacador.morte();
+                System.out.println("**********");
+
+            }
+            if (!campeao) {
+                if (cacador.estaMorto() && !tico.estaMorto()) {
+                    System.out.println(cacador.nome + " está morto!");
+                    System.out.println(tico.nome + " é o campeão!");
+                    System.out.println("**********");
+                    campeao = true;
+                } else if (!cacador.estaMorto() && tico.estaMorto()) {
+                    System.out.println(tico.nome + " está morto!");
+                    System.out.println(cacador.nome + " é o campeão!");
+                    System.out.println("**********");
+                    campeao = true;
+                }else if (cacador.estaMorto() && tico.estaMorto()) {
+                    System.out.println("Empate! Os dois morreram juntos.");
+                    campeao = true; 
+                    System.out.println("**********");
+
+                }
+            }
+            Thread.sleep(5000);
         }
+        System.out.println("Não restaram personagens vivos. GAME OVER.");
     }
 }
--- src/Personagem.java
+++ src/Personagem.java
@@ -12,7 +12,7 @@ public class Personagem {
 
     Personagem(String nome) {
         this.nome = nome;
-        System.out.println("Construindo novo personagem");
+        System.out.println("Construindo novo personagem: " + nome);
         energia = 10;
         fome = 0;
         sono = 0;
@@ -20,19 +20,18 @@ public class Personagem {
     }
 
     Personagem(String nome, int energia, int fome, int sono) {
-        System.out.println("Construindo novo personagem");
+        System.out.println("Construindo novo personagem: " + nome);
         this.nome = nome;
         this.energia = energia < 0 || energia > 10 ? 10 : energia;
         this.fome = fome >= 0 && fome <= 10 ? fome : 0;
         this.sono = sono >= 0 && sono <= 10 ? sono : 0;
     }
 
-    void morte(String nome, List<String> mochila, List<Musica> repertorio){
+    void morte(){
         if(energia <= 0){
         System.out.println(nome +  " Morreu");
         System.out.println("Inventario de " + nome + ": " + mochila);
         System.out.println("Repertorio de " + nome + ": " + repertorio);
-        System.out.println("******************************");
         }
     }
 
@@ -43,19 +42,16 @@ public class Personagem {
     void cacar() {
         if (energia >= 2) {
             System.out.printf("%s caçando\n", nome);
-            energia -= 2; // energia = energia - 2
+            energia -= 2; 
         } else {
             System.out.printf(
                     "%s sem energia para caçar\n", nome);
         }
-        // fome +=1 fome++ ++fome
         if (fome < 10)
             fome = fome + 1;
-        // sono = sono + 1 > 10 ? sono : sono + 1;
         sono = sono == 10 ? sono : sono + 1;
     }
 
-    // método comer
     void comer() {
         if (fome >= 1) {
             System.out.println(nome + " comendo");
@@ -66,7 +62,6 @@ public class Personagem {
         }
     }
 
-    // método dormir
     void dormir() {
         if (sono >= 1) {
             System.out.print(nome + " dormindo\n");
@@ -78,9 +73,8 @@ public class Personagem {
     }
 
     public String toString() {
-        // nome: e:5, f:4, s:8
         return String.format(
-                "%s: e:%d, f:%d, s:%d\nmochila:%s\nrepertorio:%s\n******************************",
+                "%s: e:%d, f:%d, s:%d\nmochila:%s\nrepertorio:%s\n",
                 nome, energia, fome, sono, mochila, repertorio);
     }
 
@@ -99,4 +93,36 @@ public class Personagem {
     void adicionaMusica (Musica musica) {
         repertorio.add(musica);
     }
+
+    boolean conheceMusica (Musica musica) {
+        for (int i = 0; i < repertorio.size(); i++) {
+            if (repertorio.get(i).getTitulo().equals(musica.getTitulo())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+   void dueloMusical (Personagem rival) {
+        Random gerador = new Random();
+        if (repertorio.size() == 0) {
+            System.out.println(nome + " não conhece nenhuma música. Duelo encerrado.");
+        } else {
+            int indice = gerador.nextInt(repertorio.size());
+            Musica sorteada = repertorio.get(indice);
+            System.out.println(nome + " iniciou o duelo com: " + sorteada.getTitulo());
+            if (rival.conheceMusica(sorteada)) {
+                energia = energia - 1;
+                rival.energia = rival.energia - 1;
+                System.out.println(rival.nome + " já conhece a música.");
+                System.out.println("Ambos perdem 1 ponto de energia.");
+            } else {
+                rival.energia = rival.energia - 1;
+                rival.repertorio.add(sorteada);
+                System.out.println(rival.nome + " não conhece a música.");
+                System.out.println(rival.nome + " perdeu 1 ponto de energia, mas aprendeu a música: " + sorteada.getTitulo());
+            }
+        }
+    }
+
 }
